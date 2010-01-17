d20c801ec6c867b00e3cbfe6f411c426e24385ef
Simon Coggins <simoncoggins@gmail.com>
2010-01-17 20:38:52 +1300

    Added sort order and method for getting current tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -10,7 +10,9 @@
 #
 
 class Tag < ActiveRecord::Base
-  has_many :tag_assignments, :dependent => :destroy
-  has_many :transactions, :through => :tag_assignments
+  has_many :tag_assignments, :dependent => :destroy,
+    :order => "source ASC, updated_at DESC"
+  has_many :transactions, :through => :tag_assignments,
+    :order => "source ASC, updated_at DESC"
   validates_presence_of :name
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -12,12 +12,20 @@
 #
 
 class Transaction < ActiveRecord::Base
-  has_many :tag_assignments, :dependent => :destroy
-  has_many :tags, :through => :tag_assignments
+  has_many :tag_assignments, :dependent => :destroy, 
+    :order => "source ASC, updated_at DESC"
+  has_many :tags, :through => :tag_assignments, 
+    :order => "source ASC, updated_at DESC"
   attr_accessible :date, :amount, :text
 
   validates_presence_of :date, :amount
-  # how to check for float?
   validates_numericality_of :amount
+
+  # could be done more efficiently with
+  # extra field in transaction updated by
+  # after_save method in tag_assignment
+  def tag
+    self.tags.find(:first)
+  end
 
 end
